765c395ef47e6f5578d42f2d880956957ca7e8f1
Thalisson Schumaher <tdasilva@corp.optaresolutions.com>
2023-10-25 16:06:08 +0100

    ss

--- mailSender.py
+++ mailSender.py
@@ -1,18 +1,27 @@
 import smtplib
 import os
+import email.message
 
 # Get email credentials and other information from environment variables
 from_email = os.environ.get('EMAIL_USERNAME')
 password = os.environ.get('EMAIL_PASSWORD')
 to_email = os.environ.get('TO_EMAIL')
-message = 'teste1'
+message = os.environ.get('MESSAGE')
 
+
+if from_email is None or to_email is None or message is None or password is None:
+    print("Please set the environment variables: FROM_EMAIL, TO_EMAIL, EMAIL_MESSAGE, and EMAIL_PASSWORD")
+    exit(1)
+
+# Create an email message
+msg = email.message.Message()
+msg.set_payload(message)
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
 server.ehlo()
 server.login(from_email, password)
-server.sendmail(from_email, to_email, message)
+server.sendmail(from_email, to_email, msg.as_string())
 server.rset()
 server.quit()
